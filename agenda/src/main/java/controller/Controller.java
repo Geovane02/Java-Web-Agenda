6076578ea373ddf4416e68cc96a1b5b751671aaf
Geovane Novak <87618732+Geovane02@users.noreply.github.com>
2021-08-30 20:17:38 -0300

    Validação de formulários, conectado Arquivo DAO e Controller com banco de dados.
    Fazendo a conexão com o banco de dados para transferir os dados do arquivo novo.html para o BD, com isso foi possível fazer o primeiro cadastro de um cliente e armazenar no BD.

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -25,13 +27,38 @@ public class Controller extends HttpServlet {
 		System.out.println(action);
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if (action.equals("/insert")){
+			novoContato(request,response);
+		}else {
+			response.sendRedirect("index.html");
 		}
 		// teste de conexao
 		// dao.testeConexao();
 	}
-
+	//listar contato
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
 	}
+	//novo Contato
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//teste de recebimento de dados
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("fone"));
+		System.out.println(request.getParameter("email"));
+		
+		//setar as variaveis javaBeans
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		//invocar classe responsavel pela inserção do banco de DADOS
+		dao.inserirContato(contato);
+		//redirecionar para o documento agenda.jsp
+		response.sendRedirect("main");
+	}
+	
+	
+	
+	
 }
--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -2,36 +2,58 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DAO {
 	/** Módulo de Conexão **/
-	//Parâmetros de conexão
+	// Parâmetros de conexão
 	private String driver = "com.mysql.jdbc.Driver";
 	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
 	private String user = "root";
 	private String password = "281024";
-	
-	
-	//Método de conexão
+
+	// Método de conexão
 	private Connection conectar() {
 		Connection con = null;
 		try {
-			Class.forName(driver); //vai ler o driver do banco de dados
+			Class.forName(driver); // vai ler o driver do banco de dados
 			con = DriverManager.getConnection(url, user, password);
 			return con;
 		} catch (Exception e) {
 			System.out.println(e);
 			return null;
-			}
+		}
 	}
-		//teste de conexão
-	//public void testeConexao() {
-		//try {
-		//	Connection con = conectar();
-		//	System.out.println(con);
-		//	con.close();
-		//} catch (Exception e) {
-		//	System.out.println(e);
-		//}
-	//}
+	// teste de conexão
+	// public void testeConexao() {
+	// try {
+	// Connection con = conectar();
+	// System.out.println(con);
+	// con.close();
+	// } catch (Exception e) {
+	// System.out.println(e);
+	// }
+	// }
+
+	/** CRUD CREAT **/
+	public void inserirContato(JavaBeans contato) {
+		String create = "insert into contatos (nome,fone,email) values (?,?,?)";
+		try {
+			// abrir a conexão com o banco
+			Connection con = conectar();
+			// preparar a query para execução no banco de dados
+			PreparedStatement pst = con.prepareStatement(create);
+			// Substituir os parametros (?) pelo conteúdo das variaveis JavaBeans
+			pst.setString(1, contato.getNome());
+			pst.setString(2, contato.getFone());
+			pst.setString(3, contato.getEmail());
+			// Executar a query
+			pst.executeUpdate();
+			// Encerrar a conexão com o Banco de Dados
+			con.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
